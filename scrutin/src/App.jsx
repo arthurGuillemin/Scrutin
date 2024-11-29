cc9c542c3b4832fc61b8a24e6708e4e87e3143e8
arthurGuillemin <aguillemin291@gmail.com>
2024-11-29 15:50:34 +0100

    Routes react router

--- scrutin/src/App.jsx
+++ scrutin/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 import CreateScrutin from "./pages/CreateScrutin.jsx";
+import Login from "./pages/login.jsx";
+import CreateAccount from "./pages/createAccount.jsx";
+import HomePage from './pages/home';
 
 
 function App() {
@@ -12,6 +15,12 @@ function App() {
       <Header isAuthenticated={isAuthenticated} />
       <Routes>
         <Route path="/creation" element={<CreateScrutin />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/createAccount" element={<CreateAccount />} />
+        <Route path="/home" element={<HomePage />} />
+
+
+
       </Routes>
     </Router>
   );
